6afb25bacb9a8ad7a41d18a8942147e96b5d343f
Bjoern Koehn <bjoern.koehn@web.de>
2020-06-13 20:48:51 +0200

    #0 -- fix violations of PEP8 naming conventions

--- mou_ec_estimation.py
+++ mou_ec_estimation.py
@@ -1,4 +1,5 @@
 import os
+import itertools
 import scipy.io
 import scipy.stats as stt
 import numpy as np
@@ -10,6 +11,7 @@ _I_REST_RUN = 0
 _I_NBACK_RUN = 1
 _I_NO_TIMESHIFT = 0
 _I_ONE_TIMESHIFT = 1
+_SUBJECT_AXIS = 0
 
 plt.close('all')
 
@@ -42,7 +44,8 @@ for k in range(len(run)):
         regionalized_preprocessed_fMRI_data[run[k]], (2, 1, 0))
 
 ## Calculate functional connectivity (BOLD covariances) [Q0 und Q1].
-time_shift = np.arange(4, dtype=float)
+# time_shift = np.arange(4, dtype=float) # for autocovariance plots
+time_shift = np.arange(2, dtype=float) 
 n_shifts = len(time_shift)
 
 FC_emp = np.zeros([n_subjects, n_runs, n_shifts, n_rois, n_rois])
@@ -62,9 +65,10 @@ for i_subject in range(n_subjects):
                                              i_shift:n_ts_span+i_shift],
                              axes=(1, 1)) / float(n_ts_span - 1)
 
-rescale_FC_factor = (0.5 / FC_emp[:, _I_REST_RUN, _I_NO_TIMESHIFT, :, 
+rescale_FC_factor = (0.5 / FC_emp[:, _I_REST_RUN, _I_NO_TIMESHIFT, :,
                                   :].diagonal(axis1=1, axis2=2).mean())
 FC_emp *= rescale_FC_factor
+filtered_ts_emp /= np.sqrt(0.11)
 
 print('most of the FC values should be between 0 and 1')
 print('mean FC0 value:', FC_emp[:, :, _I_NO_TIMESHIFT, :, :].mean(),
@@ -82,25 +86,27 @@ plt.ylabel('matrix element count', fontsize=14)
 plt.title('distribution of FC0 values')
 # Show FC0 averaged over subjects first run (rest).
 plt.figure()
-FC_avg_over_subj = FC_emp[:, _I_REST_RUN, _I_NO_TIMESHIFT, :, :].mean(axis=0)
+FC_avg_over_subj = FC_emp[:, _I_REST_RUN, 
+                          _I_NO_TIMESHIFT, :, :].mean(axis=_SUBJECT_AXIS)
 plt.imshow(FC_avg_over_subj, origin='lower', cmap='Blues', vmax=1, vmin=0)
 plt.colorbar()
-plt.xlabel('source ROI', fontsize=14)
-plt.ylabel('target ROI', fontsize=14)
+plt.xlabel('target ROI', fontsize=14)
+plt.ylabel('source ROI', fontsize=14)
 plt.title('FC0 (functional connectivity with no time lag)')
 # Show FC1 averaged over subjects first run (rest).
 plt.figure()
-FC_avg_over_subj = FC_emp[:, _I_REST_RUN, _I_ONE_TIMESHIFT, :, :].mean(axis=0)
+FC_avg_over_subj = FC_emp[:, _I_REST_RUN, 
+                          _I_ONE_TIMESHIFT, :, :].mean(axis=_SUBJECT_AXIS)
 plt.imshow(FC_avg_over_subj, origin='lower', cmap='Blues', vmax=1, vmin=0)
 plt.colorbar()
-plt.xlabel('source ROI', fontsize=14)
-plt.ylabel('target ROI', fontsize=14)
+plt.xlabel('target ROI', fontsize=14)
+plt.ylabel('source ROI', fontsize=14)
 plt.title('FC1 (functional connectivity with time lag 1TR)')
 # Show the autocovariance for the first run (rest).
 ac = FC_emp.diagonal(axis1=3, axis2=4)
 plt.figure()
-ac_avg_over_subj = np.log(np.maximum(ac[:, _I_REST_RUN, :, :].mean(axis=0),
-                                     np.exp(-4.0)))
+ac_avg_over_subj = np.log(np.maximum(ac[:, _I_REST_RUN, :, :].
+                                     mean(axis=_SUBJECT_AXIS), np.exp(-4.0)))
 plt.plot(range(n_shifts), ac_avg_over_subj)
 plt.xlabel('time lag', fontsize=14)
 plt.ylabel('log autocovariance', fontsize=14)
@@ -109,8 +115,8 @@ plt.ylim((-3, 0))
 plt.xlim((0, 3))
 # Show the autocovariance for the 2nd run (nBack).
 plt.figure()
-ac_avg_over_subj = np.log(np.maximum(ac[:, _I_NBACK_RUN, :, :].mean(axis=0),
-                                     np.exp(-3.1)))
+ac_avg_over_subj = np.log(np.maximum(ac[:, _I_NBACK_RUN, :, :].
+                                     mean(axis=_SUBJECT_AXIS), np.exp(-3.1)))
 plt.plot(range(n_shifts), ac_avg_over_subj)
 plt.xlabel('time lag', fontsize=14)
 plt.ylabel('log autocovariance', fontsize=14)
@@ -129,8 +135,8 @@ for i in range(int(n_rois/2)):
 # Visualise the structural connectivity mask.
 plt.figure()
 plt.imshow(mask_EC, origin='lower')
-plt.xlabel('Source ROI', fontsize=14)
-plt.ylabel('Target ROI', fontsize=14)
+plt.xlabel('target ROI', fontsize=14)
+plt.ylabel('source ROI', fontsize=14)
 plt.title('Mask for existing connections', fontsize=12)
 
 ## Calculate EC-matrix.
@@ -138,7 +144,7 @@ plt.title('Mask for existing connections', fontsize=12)
 # (here, no input cross-correlation).
 mask_Sigma = np.eye(n_rois, dtype=bool)
 # Run the model optimization.
-# Initialize the target arrays.
+# Initialize the source arrays.
 
 # Jacobian (off-diagonal elements = EC)
 J_mod = np.zeros([n_subjects, n_runs, n_rois, n_rois])
@@ -194,11 +200,11 @@ print('\nFinished.')
 
 # Plot C-matrix for resting state data.
 plt.figure()
-plt.imshow(C_mod[:, _I_REST_RUN, :, :].mean(axis=0), origin='lower',
-           cmap='Reds')
+plt.imshow(C_mod[:, _I_REST_RUN, :, :].mean(axis=_SUBJECT_AXIS), 
+           origin='lower', cmap='Reds')
 plt.colorbar()
-plt.xlabel('source ROI', fontsize=14)
-plt.ylabel('target ROI', fontsize=14)
+plt.xlabel('target ROI', fontsize=14)
+plt.ylabel('source ROI', fontsize=14)
 plt.title('effective connectivity C_{ij}')
 plt.show()
 
@@ -206,44 +212,79 @@ plt.show()
 local_var = np.zeros([n_runs, n_rois])
 mean_rc_var = np.zeros([n_runs])
 mean_periph_var = np.zeros([n_runs])
-confInt = np.zeros([n_runs, 2])
+conf_int_rc = np.zeros([n_runs, 2])
+conf_int_periph = np.zeros([n_runs, 2])
 # Create a 1D-mask for rich club regions.
-maskRichClub = np.zeros(n_rois, dtype=bool)
-indexesRichClub = [23, 26, 27, 57, 60, 61]
-maskRichClub[indexesRichClub] = True
+mask_rc = np.zeros(n_rois, dtype=bool)
+indexes_rich_club = [23, 26, 27, 57, 60, 61]
+mask_rc[indexes_rich_club] = True
 print('rich club regions: '
-      + str(np.concatenate(roi_labels[indexesRichClub]).tolist()))
+      + str(np.concatenate(roi_labels[indexes_rich_club]).tolist()))
 
 for i_run in range(n_runs):
-    # Average over subjects.
-    local_var[i_run, :] = (Sigma_mod[:, i_run, :, :].mean(axis=0).diagonal())
-    mean_rc_var[i_run] = local_var[i_run, maskRichClub].mean()
-    mean_periph_var[i_run] = local_var[i_run, ~maskRichClub].mean()
-    mean, sigma = local_var[i_run, :].mean(), local_var[i_run, :].std(ddof=1)
-    confInt[i_run, :] = stt.norm.interval(0.95, loc=mean, scale=sigma)
-print('95% Konfidenz Interval: ' + str(confInt))
+    local_var[i_run, :] = (Sigma_mod[:, i_run, :, :].
+                           mean(axis=_SUBJECT_AXIS).diagonal())
+    mean_rc_var[i_run] = local_var[i_run, mask_rc].mean()
+    mean_periph_var[i_run] = local_var[i_run, ~mask_rc].mean()
+    sigma_rc = local_var[i_run, mask_rc].std(ddof=1)
+    sigma_periph = local_var[i_run, ~mask_rc].std(ddof=1)
+    conf_int_rc[i_run, :] = stt.norm.interval(0.95, 
+                                              loc=mean_rc_var[i_run], 
+                                              scale=sigma_rc)
+    conf_int_periph[i_run, :] = stt.norm.interval(0.95, 
+                                                  loc=mean_periph_var[i_run],
+                                                  scale=sigma_periph)
+print('95% Konfidenz Interval (rich cluc): ' + str(conf_int_rc))
+print('95% Konfidenz Interval (periphery): ' + str(conf_int_periph))
+print('Mittel der lokalen Variabilität (rich club): ' + str(mean_rc_var))
+print('Mittel der lokalen Variabilität (periphery): ' + str(mean_periph_var))
+
+local_var_avg = np.zeros([n_runs, n_rois, n_rois])
+mask_rc_connections = np.zeros([n_rois, n_rois], dtype=bool)
+mask_rc_connections[indexes_rich_club, indexes_rich_club] = True
+rc_ind_combin = np.array(list(itertools.permutations(indexes_rich_club, 
+                                                        2))).T
+mask_rc_connections[rc_ind_combin[0], rc_ind_combin[1]] = True
+for i_run in range(n_runs):
+    local_var_avg[i_run, :, :] = Sigma_mod[:, i_run, :, :].mean(axis=_SUBJECT_AXIS)
+    mean_rc_var[i_run] = local_var_avg[i_run, mask_rc_connections].mean()
+    mean_periph_var[i_run] = local_var_avg[i_run, ~mask_rc_connections].mean()
+    
+    meanRc = local_var_avg[i_run, mask_rc_connections].mean()
+    meanPeriph = local_var_avg[i_run, ~mask_rc_connections].mean()
+    sigmaRc = local_var_avg[i_run, mask_rc_connections].std(ddof=1)
+    sigmaPeriph = local_var_avg[i_run, ~mask_rc_connections].std(ddof=1)
+    conf_int_rc[i_run, :] = stt.norm.interval(0.95, loc=meanRc, scale=sigmaRc)
+    conf_int_periph[i_run, :] = stt.norm.interval(0.95, loc=meanPeriph, 
+                                            scale=sigmaPeriph)
+print('95% Konfidenz Interval (rich cluc): ' + str(conf_int_rc))
+print('95% Konfidenz Interval (periphery): ' + str(conf_int_periph))
 print('Mittel der lokalen Variabilität (rich club): ' + str(mean_rc_var))
 print('Mittel der lokalen Variabilität (periphery): ' + str(mean_periph_var))
 
 ## Calculate the input-output ratio.
 # Create a 2D-mask for rich club regions.
-maskRichClubConnections = np.zeros([n_rois, n_rois], dtype=bool)
-maskRichClubConnections[indexesRichClub, indexesRichClub] = True
+mask_rc_connections = np.zeros([n_rois, n_rois], dtype=bool)
+# The entries on the diagonal of C are 0 anyway, so that they can be
+# ignored when it comes to the mask:
+# mask_rc_connections[indexes_rich_club, indexes_rich_club] = True
+rc_ind_combin = np.array(list(itertools.permutations(indexes_rich_club, 
+                                                        2))).T
+mask_rc_connections[rc_ind_combin[0], rc_ind_combin[1]] = True
 
-roiInput = np.zeros([n_runs, n_rois])
-roiOutput = np.zeros([n_runs, n_rois])
-ioRatioRichClub = np.zeros([n_runs])
-ioRatioPeriphery = np.zeros([n_runs])
+roi_input = np.zeros([n_runs, n_rois])
+roi_output = np.zeros([n_runs, n_rois])
+io_ratio_rc = np.zeros([n_runs])
+io_ratio_periph = np.zeros([n_runs])
 for i_run in range(n_runs):
     # Examine input-output ratio ignoring inter-rich-club connections.
-    # Average over subjects.
-    noRichClubConnections_C = (C_mod[:, i_run, :, :].mean(axis=0))
-    noRichClubConnections_C[maskRichClubConnections] = 0
-    roiInput[i_run, :] = noRichClubConnections_C[:, :].sum(axis=1)
-    roiOutput[i_run, :] = noRichClubConnections_C[:, :].sum(axis=0)
-    ioRatioRichClub[i_run] = (roiInput[i_run, maskRichClub].sum() /
-                              roiOutput[i_run, maskRichClub].sum())
-    ioRatioPeriphery[i_run] = (roiInput[i_run, ~maskRichClub].sum() /
-                               roiOutput[i_run, ~maskRichClub].sum())
-print('input-output ratio rich club: ', str(ioRatioRichClub))
-print('input-output ratio periphery: ', str(ioRatioPeriphery))
+    no_rc_connections_C = C_mod[:, i_run, :, :].mean(axis=_SUBJECT_AXIS)
+    no_rc_connections_C[mask_rc_connections] = 0
+    roi_input[i_run, :] = no_rc_connections_C[:, :].sum(axis=_SUBJECT_AXIS)
+    roi_output[i_run, :] = no_rc_connections_C[:, :].sum(axis=1)
+    io_ratio_rc[i_run] = (roi_input[i_run, mask_rc].sum() /
+                              roi_output[i_run, mask_rc].sum())
+    io_ratio_periph[i_run] = (roi_input[i_run, ~mask_rc].sum() /
+                               roi_output[i_run, ~mask_rc].sum())
+print('input-output ratio rich club: ', str(io_ratio_rc))
+print('input-output ratio periphery: ', str(io_ratio_periph))
